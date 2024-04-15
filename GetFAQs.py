82417c2bbc0afb26b680269266cdf33177746e50
kamtendra-myoperator <158162523+kamtendra-myoperator@users.noreply.github.com>
2024-04-15 21:26:47 +0530

    Update GetFAQs.py

--- GetFAQs.py
+++ GetFAQs.py
@@ -16,7 +16,7 @@ def generate_questions_and_answers(result_string):
     6. Do not include Customers's company's information in the FAQs.
     Always return the data in JSON format given {"data":[{"Question":"<Question>","Answer":"<Answer>"},{"Question":"<Question>","Answer":"<Answer>"}]}'''
 
-    client = OpenAI(api_key="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")
+    client = OpenAI(api_key="YOUR_API_KEY")
 
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",  # You can adjust the model based on your preference
